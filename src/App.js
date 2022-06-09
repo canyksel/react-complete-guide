6903f97b2643560b9a711af8835874327c290a07
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2022-06-10 01:22:46 +0300

    finished: working with states and events

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -28,6 +29,12 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense =>{
+    console.log('In App.js');
+    console.log(expense);
+
+  };
+  
   //JSX explanation
   // return React.createElement(
   //   "div",
@@ -38,7 +45,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
