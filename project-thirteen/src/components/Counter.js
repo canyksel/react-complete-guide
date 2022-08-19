5d1905b9050488f84e54f2971d82f0e08a6bcac0
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2022-08-19 22:38:44 +0300

    lecture-18 part-6

--- project-thirteen/src/components/Counter.js
+++ project-thirteen/src/components/Counter.js
@@ -1,12 +1,29 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispath = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispath({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispath({ type: "decrement" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
