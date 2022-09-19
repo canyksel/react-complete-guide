e3e268129e80e2608fa246297a956af1bee188f9
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2022-09-19 23:31:36 +0300

    lecture 20 part-7.2

--- project-sixteen/src/App.js
+++ project-sixteen/src/App.js
@@ -1,8 +1,30 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
+
 function App() {
   return (
-    <div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
 
-    </div>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
