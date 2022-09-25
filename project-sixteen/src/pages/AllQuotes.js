ba071efa5c13890b6faca9fc5dfea33662442c23
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2022-09-25 23:57:21 +0300

    lecture 20 part-10.2

--- project-sixteen/src/pages/AllQuotes.js
+++ project-sixteen/src/pages/AllQuotes.js
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
 import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuetesFound from "../components/quotes/NoQuotesFound";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Can", text: "Learning React is fun!" },
@@ -6,7 +11,34 @@ const DUMMY_QUOTES = [
 ];
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focues">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuetesFound />;
+  }
+
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
