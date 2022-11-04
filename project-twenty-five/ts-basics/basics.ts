868ebf7e884e530eb157d0df143cf15774ddecf1
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2022-11-04 13:54:28 +0300

    lecture-27 part-2

--- project-twenty-five/ts-basics/basics.ts
+++ project-twenty-five/ts-basics/basics.ts
@@ -19,9 +19,9 @@ let hobbies: string[];
 hobbies = ["Sports", "Cooking"];
 
 type Person = {
-    name: string;
-    age: number;
-}
+  name: string;
+  age: number;
+};
 
 let person: Person;
 
@@ -34,27 +34,45 @@ let person: Person;
 person = {
   name: "Can",
   age: 29,
-  
 };
 
 //person = {
-//    isEmployee: true
+//    isEmployee: true-
 //};
-
-
 
 //Array object
 //let people: {
 //    name: string;
 //    age: number;
-//}[];   
+//}[];
 
-let people : Person[];
+let people: Person[];
 
 //Type inference
-let course: string | number = 'React - The Complete Guide';
+let course: string | number = "React - The Complete Guide";
 
 course = 123456;
 
+//Functions & types
 
+function addNumbers(a: number, b: number) {
+  return a + b;
+}
 
+function print(value: any) {
+  console.log(value);
+}
+
+//Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1,1,2,3]
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
+
+//updatedArray[0].split("");
