e20c4bb86789bd0a0431ef62d8c75dcc38559103
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2022-11-07 21:45:36 +0300

    lecture-27 part-5

--- project-twenty-five/react-ts/src/App.tsx
+++ project-twenty-five/react-ts/src/App.tsx
@@ -2,16 +2,23 @@ import Todos from './components/Todos';
 import Todo from './modals/todo';
 
 import './App.css';
+import NewTodo from './components/NewTodo';
+import { useState } from 'react';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
+  const addTodoHandler = (todoText: string) =>{
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) =>{
+      return prevTodos.concat(newTodo);
+    });
+  }
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- project-twenty-five/react-ts/src/components/NewTodo.tsx
+++ project-twenty-five/react-ts/src/components/NewTodo.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
+import classes from './NewTodo.module.css';
 
-const NewTodo = () => {
+const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -12,10 +13,13 @@ const NewTodo = () => {
       //throw error
       return;
     }
+
+    props.onAddTodo(enteredText);
+
   };
   
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
