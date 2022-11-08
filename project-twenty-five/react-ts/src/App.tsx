bf8edc3752514ca5cdb5f46d17fe2cc96c8fab69
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2022-11-08 23:43:07 +0300

    lecture-27 part-6

--- project-twenty-five/react-ts/src/App.tsx
+++ project-twenty-five/react-ts/src/App.tsx
@@ -1,25 +1,31 @@
-import Todos from './components/Todos';
-import Todo from './modals/todo';
+import Todos from "./components/Todos";
+import Todo from "./modals/todo";
 
-import './App.css';
-import NewTodo from './components/NewTodo';
-import { useState } from 'react';
+import "./App.css";
+import NewTodo from "./components/NewTodo";
+import { useState } from "react";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (todoText: string) =>{
+  const addTodoHandler = (todoText: string) => {
     const newTodo = new Todo(todoText);
 
-    setTodos((prevTodos) =>{
+    setTodos((prevTodos) => {
       return prevTodos.concat(newTodo);
     });
-  }
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
 
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
