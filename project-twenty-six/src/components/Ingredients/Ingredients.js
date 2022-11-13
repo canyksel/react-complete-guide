9e079c4ecd80e5b6bd6e2e1474fe1b081b481e33
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2022-11-13 23:08:30 +0300

    lecture-28 part-3

--- project-twenty-six/src/components/Ingredients/Ingredients.js
+++ project-twenty-six/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,77 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+ // useEffect(() => {
+ //   fetch(
+ //     "https://react-prep-e7c98-default-rtdb.firebaseio.com//ingredients.json"
+ //   ).then((response) =>
+ //     response.json().then((responseData) => {
+ //       const loadedIngredients = [];
+ //       for (const key in responseData) {
+ //         loadedIngredients.push({
+ //           id: key,
+ //           title: responseData[key].title,
+ //           amount: responseData[key].amount,
+ //         });
+ //       }
+ //       setUserIngredients(loadedIngredients);
+ //     })
+ //   );
+ // }, []);
+
+  useEffect(() => {
+    console.log("RENDERIN INGREDIENTS", userIngredients);
+  }, [userIngredients]);
+
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    setUserIngredients(filteredIngredients);
+  }, []);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://react-prep-e7c98-default-rtdb.firebaseio.com//ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
