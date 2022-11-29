dd4fed57bedf4a6dcd9f8385e83b43ac8b1262f0
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2022-11-30 00:40:45 +0300

    lecture-29 finished

--- project-twenty-eight/src/components/meetups/NewMeetupForm.js
+++ project-twenty-eight/src/components/meetups/NewMeetupForm.js
@@ -23,7 +23,7 @@ export default function NewMeetupForm(props) {
       description: enteredDescription,
     };
 
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
 
   return (
